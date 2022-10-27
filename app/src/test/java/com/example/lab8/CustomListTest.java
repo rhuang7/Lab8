a9f539f32ada0e2617dfc4fca287a5c6c8944de1
rhuang7 <rhuang7@ualberta.ca>
2022-10-27 17:21:10 -0600

    test

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -13,4 +13,13 @@ import java.util.ArrayList;
 public class CustomListTest {
 
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
 }
